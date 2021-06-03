4fd5b8523facb423e805ed6782ac6bf557fbf4b4
FlutLab.io <>
2021-06-03 14:46:55 +0000

    Autosave

--- lib/api/loripsum_api.dart
+++ lib/api/loripsum_api.dart
@@ -3,9 +3,11 @@ import 'package:http/http.dart' as http;
 
 class LoripsumApi {
   static Future<String> getLoripsum() async {
-    var url = Uri.parse('https://loripsum.net/api/10/short/headers');
+    print('Entrou');
+    var url = Uri.parse('https://loripsum.net/api/');
     var response = await http.get(url);
     var text = response.body;
+    print('saiu');
 
     text = text.replaceAll('<p>', '');
     text = text.replaceAll('</p>', '');
