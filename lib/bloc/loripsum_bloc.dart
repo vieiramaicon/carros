1744ffc47eeb68cf557f5e2e87a0f4eb5841d719
FlutLab.io <>
2021-06-03 17:28:36 +0000

    Autosave

--- lib/bloc/loripsum_bloc.dart
+++ lib/bloc/loripsum_bloc.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
+import 'package:http/http.dart' as http;
 import '../api/loripsum_api.dart';
 import 'simple_bloc.dart';
 
 class LoripsumBloc extends SimpleBloc<String> {
   void fetch() async {
-    try{
-      var text = await LoripsumApi.getLoripsum();
+    var url = Uri.parse('https://example.com/foobar.txt');
+    var response = await http.get(url);
+    var text = response.body;
+
+    try {
+      //var text = await LoripsumApi.getLoripsum();
       add(text);
-    } catch(e) {
+    } catch (e) {
       addError(e);
     }
   }
