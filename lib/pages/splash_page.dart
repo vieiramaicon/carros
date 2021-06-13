b007932eb27bdd3546d0c1a3e15b471d71e73793
FlutLab.io <>
2021-06-13 14:54:13 +0000

    Autosave

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -9,7 +9,7 @@ class _SplashPage extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue(200),
+      color: Colors.blue[200],
       child: Center( 
         child:CircularProgressIndicator(),
       )
