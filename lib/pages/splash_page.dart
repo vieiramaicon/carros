e89de2b3ad92d4f684fbb0475df9c4ae24f5bc4a
FlutLab.io <>
2021-06-14 01:16:06 +0000

    versão 2

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -19,8 +19,8 @@ class _SplashPage extends State<SplashPage> {
     Future<Database> futureDB = CarroDAO().db;
     Future futureTime = Future.delayed(Duration(seconds: 3));
     Future<Usuario> futureUser = Usuario.get();
-    Future.wait([futureTime, futureUser, futureDB]).then((values) {
-      var user = values[1];
+    Future.wait([futureDB, futureTime, futureUser]).then((values) {
+      var user = values[2];
       if(user != null) {
         Navigator.of(context).pushReplacementNamed('/HomePage');
       } else {
